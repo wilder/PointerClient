554b9ccc2aa9a74cf22222ac2556bcb1d45b6373
Wilder Pereira <wilder_roberto@hotmail.com>
2019-01-07 23:04:25 -0200

    Remove hardcoded coordinate type

--- src/main/java/com/wilderpereira/presenters/MainPresenter.kt
+++ src/main/java/com/wilderpereira/presenters/MainPresenter.kt
@@ -46,7 +46,6 @@ class MainPresenter : MainContract.Presenter {
         when (coordinate.type) {
             "point" -> view.displayPoint(coordinate)
             "focus" -> view.focus(coordinate)
-            else -> view.focus(coordinate)
         }
     }
 
